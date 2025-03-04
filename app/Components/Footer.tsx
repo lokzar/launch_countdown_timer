18e22e4b12660fb69633f3678f56adf24d05e3a7
lokzar <motart2505@gmail.com>
2025-03-03 23:46:34 -0600

    added svg compatibility

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,16 +1,19 @@
+import FacebookIcon from "@/public/icons/icon-facebook.svg";
+import InstagramIcon from "@/public/icons/icon-instagram.svg";
+import PinterestIcon from "@/public/icons/icon-pinterest.svg";
 
 const Footer = () => {
     return(
         <footer className="absolute bg-cover bottom-0 w-full h-50 bg-[url('/assets/pattern-hills.svg')]">
         <div className="w-full flex justify-center space-x-10 pt-24">
           <a href="https://www.facebook.com">
-            <img className="" src="/icons/icon-facebook.svg" alt= "facebook svg"/>
+            <FacebookIcon className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
           </a>
           <a href="https://www.pinterest.com">
-            <img className="" src="/icons/icon-pinterest.svg" alt= "facebook svg"/>
+            <PinterestIcon className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
           </a>
           <a href="https://www.instagram.com">
-            <img className="" src="/icons/icon-instagram.svg" alt= "facebook svg"/>
+            <InstagramIcon className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
           </a>
         </div>
       </footer>
