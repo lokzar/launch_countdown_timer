2da3a190d06c66279211b6f96cb847cbdee690ad
lokzar <motart2505@gmail.com>
2025-03-04 16:11:11 -0600

    number top and bottom container

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Red_Hat_Text } from "next/font/google";
 import "./globals.css";
 
 //fonts
-const redhattext = Red_Hat_Text({weight:["700"], subsets:["latin"]})
+const redhattext = Red_Hat_Text({weight:["600", "700"], subsets:["latin"]})
 
 export const metadata: Metadata = {
   title: "Countdown",
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,34 @@ import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
-    <div className="relative h-screen flex flex-col items-center bg-gradient-to-t from-[#493349] via-[#211d2b] to-[#251e2f]">
+    <div className="relative h-screen flex flex-col items-center bg-gradient-to-t from-[#493349] via-[#211d2b] to-[#1e1e28]">
       <div className="absolute inset-0 bg-[url('/assets/bg-stars.svg')] bg-cover bg-center opacity-80"></div>
       <div className="flex-grow">
-        <h1 className="text-white text-sm md:text-xl lg:text-3xl font-bold capitalize tracking-[10] pt-20">
+        <h1 className="text-white text-lg text-center md:text-xl lg:text-3xl capitalize tracking-[9] pt-40">
           WE&apos;RE LAUNCHING SOON
         </h1>
         
-        <div>
 
+        <div className="flex-col items-center gap-[1rem]">
+          <div className="relative h-[1.8em] inline-flex flex-col w-full min-w-[75px] sm:min-w-[100px] md:min-w-[150px] text-[1.75rem] sm:text-[3rem] md:text-[5rem]">
+            {/* Top Container */}
+            <div className="relative h-[50%] bg-[#2c2c44] rounded-lg">
+              {/* Top Number position */}
+              <div className="relative h-full px-1 pt-[0.4em] overflow-hidden leading-none rounded-t-md text-[#d4506f] text-center">
+                <div> 3 </div>
+              </div>
+            </div>
+            {/* Bottom Container */}
+            <div className="relative h-[50%] bg-[#34364f] rounded-lg">
+              {/* Bottom Number position */}
+              <div className="relative flex justify-center items-end h-full pb-[0.4em] overflow-hidden leading-none rounded-b-md text-[#d4506f] text-center w-full">
+                <div> 3 </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            days
+          </div>
         </div>
 
       </div>
