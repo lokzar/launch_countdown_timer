8afbb039927e15732d349fb731fbcc50b72a8354
lokzar <motart2505@gmail.com>
2025-03-04 19:28:44 -0600

    Some responsive adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -8,29 +8,103 @@ export default function Home() {
         <h1 className="text-white text-lg text-center md:text-xl lg:text-3xl capitalize tracking-[9] pt-40">
           WE&apos;RE LAUNCHING SOON
         </h1>
-        
-
-        <div className="flex-col items-center gap-[1rem]">
-          <div className="relative h-[1.8em] inline-flex flex-col w-full min-w-[75px] sm:min-w-[100px] md:min-w-[150px] text-[1.75rem] sm:text-[3rem] md:text-[5rem] rounded-md bg-[#1a1823] shadow-[0em_0.2em_0em_#1a1823]">
+      
+      
+      
+      <div className="flex flex-row justify-center items-center gap-[0.5rem] sm:gap-[0.5rem] md:gap-[1rem] lg:gap-[1.5rem] pt-20 w-full">
+      {/* DAYS */}  
+        <div className="flex-col items-center">
+          <div className="relative h-[1.8em] inline-flex flex-col w-full min-w-[75px] sm:min-w-[70px] md:min-w-[150px] text-[1.75rem] sm:text-[3rem] md:text-[5rem] rounded-md bg-[#1a1823] shadow-[0em_0.2em_0em_#1a1823]">
             {/* Top Container */}
-            <div className="relative h-[50%] bg-[#2c2c44] rounded-lg">
+            <div className="relative h-[50%] bg-[#2c2c44] rounded-md">
               {/* Top Number position */}
-              <div className="relative h-full px-1 pt-[0.4em] overflow-hidden leading-none rounded-t-md text-[#d4506f] text-center">
-                <div> 3 </div>
+              <div className="relative h-full px-1 pt-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold">
+                <div> 10 </div>
               </div>
             </div>
             {/* Bottom Container */}
-            <div className="relative h-[50%] bg-[#34364f] rounded-lg">
+            <div className="relative h-[50%] bg-[#34364f] rounded-md">
               {/* Bottom Number position */}
-              <div className="relative flex justify-center items-end h-full pb-[0.4em] overflow-hidden leading-none rounded-b-md text-[#d4506f] text-center w-full">
-                <div> 3 </div>
+              <div className="relative flex justify-center items-end h-full pb-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold w-full">
+                <div> 10 </div>
               </div>
             </div>
           </div>
-          <h3 className="uppercase text-[#7c7997] text-center tracking-[6] pt-8">
+          <h3 className="uppercase text-[#7c7997] text-center tracking-[3] sm:tracking-[3] md:tracking-[4] lg:tracking-[6] pt-8 text-[0.5rem] sm:text-[0.5rem] md:text-[0.8rem] lg:text-[1rem]">
             days
           </h3>
         </div>
+        {/* HOURS */}  
+        <div className="flex-col items-center">
+          <div className="relative h-[1.8em] inline-flex flex-col w-full min-w-[75px] sm:min-w-[100px] md:min-w-[150px] text-[1.75rem] sm:text-[3rem] md:text-[5rem] rounded-md bg-[#1a1823] shadow-[0em_0.2em_0em_#1a1823]">
+            {/* Top Container */}
+            <div className="relative h-[50%] bg-[#2c2c44] rounded-md">
+              {/* Top Number position */}
+              <div className="relative h-full px-1 pt-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold">
+                <div> 10 </div>
+              </div>
+            </div>
+            {/* Bottom Container */}
+            <div className="relative h-[50%] bg-[#34364f] rounded-md">
+              {/* Bottom Number position */}
+              <div className="relative flex justify-center items-end h-full pb-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold w-full">
+                <div> 10 </div>
+              </div>
+            </div>
+          </div>
+          <h3 className="uppercase text-[#7c7997] text-center tracking-[3] sm:tracking-[3] md:tracking-[4] lg:tracking-[6] pt-8 text-[0.5rem] sm:text-[0.5rem] md:text-[0.8rem] lg:text-[1rem]">
+            hours
+          </h3>
+        </div>
+        {/* MINUTES */}  
+        <div className="flex-col items-center">
+          <div className="relative h-[1.8em] inline-flex flex-col w-full min-w-[75px] sm:min-w-[100px] md:min-w-[150px] text-[1.75rem] sm:text-[3rem] md:text-[5rem] rounded-md bg-[#1a1823] shadow-[0em_0.2em_0em_#1a1823]">
+            {/* Top Container */}
+            <div className="relative h-[50%] bg-[#2c2c44] rounded-md">
+              {/* Top Number position */}
+              <div className="relative h-full px-1 pt-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold">
+                <div> 10 </div>
+              </div>
+            </div>
+            {/* Bottom Container */}
+            <div className="relative h-[50%] bg-[#34364f] rounded-md">
+              {/* Bottom Number position */}
+              <div className="relative flex justify-center items-end h-full pb-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold w-full">
+                <div> 10 </div>
+              </div>
+            </div>
+          </div>
+          <h3 className="uppercase text-[#7c7997] text-center tracking-[3] sm:tracking-[3] md:tracking-[4] lg:tracking-[6] pt-8 text-[0.5rem] sm:text-[0.5rem] md:text-[0.8rem] lg:text-[1rem]">
+            Minutes
+          </h3>
+        </div>
+
+       {/* SECONDS */}  
+        <div className="flex-col items-center">
+          <div className="relative h-[1.8em] inline-flex flex-col w-full min-w-[75px] sm:min-w-[100px] md:min-w-[150px] text-[1.75rem] sm:text-[3rem] md:text-[5rem] rounded-md bg-[#1a1823] shadow-[0em_0.2em_0em_#1a1823]">
+            {/* Top Container */}
+            <div className="relative h-[50%] bg-[#2c2c44] rounded-md">
+              {/* Top Number position */}
+              <div className="relative h-full px-1 pt-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold">
+                <div> 10 </div>
+              </div>
+            </div>
+            {/* Bottom Container */}
+            <div className="relative h-[50%] bg-[#34364f] rounded-md">
+              {/* Bottom Number position */}
+              <div className="relative flex justify-center items-end h-full pb-[0.4em] overflow-hidden leading-none text-[#d4506f] text-center font-bold w-full">
+                <div> 10 </div>
+              </div>
+            </div>
+          </div>
+          <h3 className="uppercase text-[#7c7997] text-center tracking-[3] sm:tracking-[3] md:tracking-[4] lg:tracking-[6] pt-8 text-[0.5rem] sm:text-[0.5rem] md:text-[0.8rem] lg:text-[1rem]">
+            Seconds
+          </h3>
+        </div>
+
+      </div>
+
+      
 
       </div>
       <Footer/>
