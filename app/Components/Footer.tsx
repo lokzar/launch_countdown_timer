86806c11ac045675f5db1b399f4931648da19448
lokzar <motart2505@gmail.com>
2025-03-05 20:58:26 -0600

    update footer html format

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -6,17 +6,23 @@ import PinterestIcon from "@/public/icons/icon-pinterest.svg";
 const Footer = () => {
     return(
       <footer className="relative bg-cover bottom-0 w-full h-[11rem] sm:h-[11rem] md:h-[12rem] lg:h-[14rem] bg-[url('/assets/pattern-hills.svg')]">
-        <div className="w-full flex justify-center pt-25 space-x-6 sm:space-x-6 md:space-x-7 lg:space-x-8">
-          <a href="https://www.facebook.com">
-            <FacebookIcon className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
-          </a>
-          <a href="https://www.pinterest.com">
-            <PinterestIcon className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
-          </a>
-          <a href="https://www.instagram.com">
-            <InstagramIcon className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
-          </a>
-        </div>
+        <ul className="w-full flex justify-center pt-25 space-x-6 sm:space-x-6 md:space-x-7 lg:space-x-8">
+          <li>
+            <a href="https://www.facebook.com" target="blank_">
+              <FacebookIcon roll="img" className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.pinterest.com" target="blank_">
+              <PinterestIcon roll="img" className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com" target="blank_">
+              <InstagramIcon roll="img" className="text-[#8285a7] w-8 h-8 hover:text-[#dc6081]"/>
+            </a>
+          </li>
+        </ul>
       </footer>
     )
 }
